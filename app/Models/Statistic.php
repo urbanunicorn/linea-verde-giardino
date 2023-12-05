c2cb89ffdb198ebe35517b957e6b55cc473106c6
guglielmopiasenti <piasentiguglielmo@gmail.com>
2023-12-05 16:09:18 +0100

    statistic model done

--- app/Models/Statistic.php
+++ app/Models/Statistic.php
@@ -8,4 +8,11 @@ use Illuminate\Database\Eloquent\Model;
 class Statistic extends Model
 {
     use HasFactory;
+
+    protected $fillable = ['product_id', 'visit_count'];
+
+    public function product()
+    {
+        return $this->belongsTo(Product::class);
+    }
 }

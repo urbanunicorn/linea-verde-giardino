af6064a6f1b6b3c38201bf33466d08983c4a3234
guglielmopiasenti <piasentiguglielmo@gmail.com>
2023-12-05 16:03:11 +0100

    category model

--- app/Models/Category.php
+++ app/Models/Category.php
@@ -8,4 +8,11 @@ use Illuminate\Database\Eloquent\Model;
 class Category extends Model
 {
     use HasFactory;
+
+    protected $fillable = ['name'];
+
+    public function subcategories()
+    {
+        return $this->hasMany(Subcategory::class);
+    }
 }

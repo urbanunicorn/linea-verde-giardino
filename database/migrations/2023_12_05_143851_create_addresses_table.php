1a5fd6bf128cec901174bbbeb27b495c407e339e
guglielmopiasenti <piasentiguglielmo@gmail.com>
2023-12-05 15:46:13 +0100

    adresses schema

--- database/migrations/2023_12_05_143851_create_addresses_table.php
+++ database/migrations/2023_12_05_143851_create_addresses_table.php
@@ -13,8 +13,22 @@ return new class extends Migration
     {
         Schema::create('addresses', function (Blueprint $table) {
             $table->id();
+            $table->foreignId('user_id')->constrained()->onDelete('cascade');
+            $table->string('first_name');
+            $table->string('last_name');
+            $table->string('company_name')->nullable();
+            $table->string('vat_number')->nullable();
+            $table->string('street_address');
+            $table->string('zipcode');
+            $table->string('city');
+            $table->string('state');
+            $table->string('country');
+            $table->string('phone');
+            $table->boolean('is_billing')->default(false);
+            $table->boolean('is_shipping')->default(false);
             $table->timestamps();
         });
+        
     }
 
     /**

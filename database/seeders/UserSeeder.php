53725ddc2a8393845d124d6948fe780b38500bd3
guglielmopiasenti <piasentiguglielmo@gmail.com>
2023-12-05 17:53:22 +0100

    user seeder done

--- database/seeders/UserSeeder.php
+++ database/seeders/UserSeeder.php
@@ -1,17 +1,22 @@
 <?php
 
-namespace Database\Seeders;
-
-use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
+use App\Models\User;
+use Faker\Generator as Faker;
+use Illuminate\Support\Facades\Hash;
 
-class UserSeeder extends Seeder
+class UsersTableSeeder extends Seeder
 {
-    /**
-     * Run the database seeds.
-     */
-    public function run(): void
+    public function run(Faker $faker)
     {
-        //
+        for ($i = 0; $i < 10; $i++) {
+            User::create([
+                'name' => $faker->name,
+                'surname' => $faker->lastName,
+                'email' => $faker->unique()->safeEmail,
+                'password' => Hash::make('password'),
+                'is_admin' => $faker->boolean,
+            ]);
+        }
     }
 }

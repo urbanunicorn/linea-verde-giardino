8c45edd24f6bca359eab8f8372da0cf39ba5bd7c
guglielmopiasenti <piasentiguglielmo@gmail.com>
2023-12-05 16:06:50 +0100

    product model done

--- app/Models/Product.php
+++ app/Models/Product.php
@@ -8,4 +8,25 @@ use Illuminate\Database\Eloquent\Model;
 class Product extends Model
 {
     use HasFactory;
+
+    protected $fillable = [
+        'subcategory_id', 'title', 'description', 'price', 'brand', 
+        'dimension', 'weight', 'is_on_sale', 'discount', 
+        'sale_start_date', 'sale_end_date', 'is_visible'
+    ];
+
+    public function subcategory()
+    {
+        return $this->belongsTo(Subcategory::class);
+    }
+
+    public function images()
+    {
+        return $this->hasMany(Image::class);
+    }
+
+    public function statistics()
+    {
+        return $this->hasOne(Statistic::class);
+    }
 }

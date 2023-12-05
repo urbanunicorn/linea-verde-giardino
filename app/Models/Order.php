24fff47039cd5efd427647ea91b6d99cd2e13b65
guglielmopiasenti <piasentiguglielmo@gmail.com>
2023-12-05 16:10:48 +0100

    order model done

--- app/Models/Order.php
+++ app/Models/Order.php
@@ -8,4 +8,20 @@ use Illuminate\Database\Eloquent\Model;
 class Order extends Model
 {
     use HasFactory;
+
+    protected $fillable = [
+        'user_id', 'address_id', 'order_date', 'total_amount', 
+        'is_completed', 'payment_method'
+    ];
+
+    public function user()
+    {
+        return $this->belongsTo(User::class);
+    }
+
+    public function address()
+    {
+        return $this->belongsTo(Address::class);
+    }
 }
+

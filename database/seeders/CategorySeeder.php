29b746dc683af86451751c4a659468c44f3e65b5
guglielmopiasenti <piasentiguglielmo@gmail.com>
2023-12-05 17:48:56 +0100

    category seeder done

--- database/seeders/CategorySeeder.php
+++ database/seeders/CategorySeeder.php
@@ -1,17 +1,18 @@
 <?php
 
-namespace Database\Seeders;
-
-use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
+use App\Models\Category;
+use Faker\Generator as Faker;
 
-class CategorySeeder extends Seeder
+class CategoriesTableSeeder extends Seeder
 {
-    /**
-     * Run the database seeds.
-     */
-    public function run(): void
+    public function run(Faker $faker)
     {
-        //
+        for ($i = 0; $i < 5; $i++) {
+            Category::create([
+                'name' => $faker->word,
+            ]);
+        }
     }
 }
+

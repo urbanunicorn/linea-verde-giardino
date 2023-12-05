4cdc90f7700286efb6b05262b9ae7fa95a322191
guglielmopiasenti <piasentiguglielmo@gmail.com>
2023-12-05 15:44:15 +0100

    products schema

--- database/migrations/2023_12_05_143851_create_products_table.php
+++ database/migrations/2023_12_05_143851_create_products_table.php
@@ -13,8 +13,21 @@ return new class extends Migration
     {
         Schema::create('products', function (Blueprint $table) {
             $table->id();
+            $table->foreignId('subcategory_id')->constrained()->onDelete('cascade');
+            $table->string('title');
+            $table->text('description');
+            $table->decimal('price', 8, 2);
+            $table->string('brand')->nullable();
+            $table->string('dimension')->nullable();
+            $table->decimal('weight')->nullable();
+            $table->boolean('is_on_sale')->default(false);
+            $table->decimal('discount')->nullable();
+            $table->date('sale_start_date')->nullable();
+            $table->date('sale_end_date')->nullable();
+            $table->boolean('is_visible')->default(true);
             $table->timestamps();
         });
+        
     }
 
     /**

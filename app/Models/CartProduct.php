cd20a082afbfadcae669d236a4a790e05181d2ac
guglielmopiasenti <piasentiguglielmo@gmail.com>
2023-12-05 16:16:50 +0100

    cart product model completed

--- app/Models/CartProduct.php
+++ app/Models/CartProduct.php
@@ -8,4 +8,16 @@ use Illuminate\Database\Eloquent\Model;
 class CartProduct extends Model
 {
     use HasFactory;
+
+    protected $fillable = ['cart_id', 'product_id', 'quantity', 'subtotal'];
+
+    public function cart()
+    {
+        return $this->belongsTo(Cart::class);
+    }
+
+    public function product()
+    {
+        return $this->belongsTo(Product::class);
+    }
 }

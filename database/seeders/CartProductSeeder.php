d2f5b2730448a1b9ad14a0660eba1428f0765043
guglielmopiasenti <piasentiguglielmo@gmail.com>
2023-12-05 17:55:09 +0100

    cart product seedeer done

--- database/seeders/CartProductSeeder.php
+++ database/seeders/CartProductSeeder.php
@@ -1,17 +1,20 @@
 <?php
 
-namespace Database\Seeders;
-
-use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
+use App\Models\CartProduct;
+use Faker\Generator as Faker;
 
-class CartProductSeeder extends Seeder
+class CartProductsTableSeeder extends Seeder
 {
-    /**
-     * Run the database seeds.
-     */
-    public function run(): void
+    public function run(Faker $faker)
     {
-        //
+        for ($i = 0; $i < 30; $i++) {
+            CartProduct::create([
+                'cart_id' => $faker->numberBetween(1, 10),
+                'product_id' => $faker->numberBetween(1, 10),
+                'quantity' => $faker->numberBetween(1, 5),
+                'subtotal' => $faker->randomFloat(2, 10, 100),
+            ]);
+        }
     }
 }

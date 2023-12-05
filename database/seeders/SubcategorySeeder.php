8eee4c6965c521766b81cfedec4f7edf3c47d7b6
guglielmopiasenti <piasentiguglielmo@gmail.com>
2023-12-05 17:49:27 +0100

    subcategory seeder done

--- database/seeders/SubcategorySeeder.php
+++ database/seeders/SubcategorySeeder.php
@@ -1,17 +1,18 @@
 <?php
 
-namespace Database\Seeders;
-
-use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
+use App\Models\Subcategory;
+use Faker\Generator as Faker;
 
-class SubcategorySeeder extends Seeder
+class SubcategoriesTableSeeder extends Seeder
 {
-    /**
-     * Run the database seeds.
-     */
-    public function run(): void
+    public function run(Faker $faker)
     {
-        //
+        for ($i = 0; $i < 10; $i++) {
+            Subcategory::create([
+                'category_id' => $faker->numberBetween(1, 5), // Assuming you have 5 categories
+                'name' => $faker->word,
+            ]);
+        }
     }
 }

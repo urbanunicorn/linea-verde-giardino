462799af12b36f0aa029359f33805d744aab7508
guglielmopiasenti <piasentiguglielmo@gmail.com>
2023-12-05 15:45:39 +0100

    users schema

--- database/migrations/2023_12_05_143851_create_users_table.php
+++ database/migrations/2023_12_05_143851_create_users_table.php
@@ -13,8 +13,14 @@ return new class extends Migration
     {
         Schema::create('users', function (Blueprint $table) {
             $table->id();
+            $table->string('name');
+            $table->string('surname');
+            $table->string('email')->unique();
+            $table->string('password');
+            $table->boolean('is_admin')->default(false);
             $table->timestamps();
         });
+        
     }
 
     /**

ba5eefcccedc28f231635edbed8fec593076d1ad
guglielmopiasenti <piasentiguglielmo@gmail.com>
2023-12-05 16:15:16 +0100

    user model completed

--- app/Models/Address.php
+++ app/Models/Address.php
@@ -8,4 +8,21 @@ use Illuminate\Database\Eloquent\Model;
 class Address extends Model
 {
     use HasFactory;
+
+    protected $fillable = [
+        'user_id', 'first_name', 'last_name', 'company_name', 
+        'vat_number', 'street_address', 'zipcode', 'city', 
+        'state', 'country', 'phone', 'is_billing', 'is_shipping'
+    ];
+
+    public function user()
+    {
+        return $this->belongsTo(User::class);
+    }
+
+    public function orders()
+    {
+        return $this->hasMany(Order::class);
+    }
 }
+

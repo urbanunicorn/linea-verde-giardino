da226735069627e2a5291de03e1a727a70278e90
matteodilorenzo21 <matteodilorenzo21@gmail.com>
2023-12-14 19:58:05 +0100

    Added Header to MainLayout

---
+++ resources/js/Layouts/MainLayout.vue
@@ -0,0 +1,34 @@
+<script setup>
+import { ref } from 'vue';
+import ApplicationLogo from '@/Components/ApplicationLogo.vue';
+import Dropdown from '@/Components/Dropdown.vue';
+import DropdownLink from '@/Components/DropdownLink.vue';
+import NavLink from '@/Components/NavLink.vue';
+import ResponsiveNavLink from '@/Components/ResponsiveNavLink.vue';
+import { Link } from '@inertiajs/vue3';
+
+import Header from '@/Components/Header.vue';
+
+const showingNavigationDropdown = ref(false);
+
+defineProps({
+    canLogin: {
+        type: Boolean = false,
+    },
+    canRegister: {
+        type: Boolean = false,
+    },
+    laravelVersion: {
+        type: String,
+        required: true,
+    },
+    phpVersion: {
+        type: String,
+        required: true,
+    },
+});
+</script>
+
+<template>
+    <Header :canLogin="canLogin" :canRegister="canRegister" :laravelVersion="laravelVersion" :phpVersion="phpVersion" />
+</template>
---
+++ resources/js/Pages/Homepage/HomePage.vue
@@ -0,0 +1,12 @@
+<script setup>
+import { Head, Link } from '@inertiajs/vue3';
+import MainLayout from '@/Layouts/MainLayout.vue';
+</script>
+
+<template>
+    <Head title="Home" />
+
+    <MainLayout />
+</template>
+
+<style></style>
